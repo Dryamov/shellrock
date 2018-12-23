928f34c97bdf0acd45b3d472160779c110c994e6
Vyacheslav Dryamov <dryamov.dev@gmail.com>
2018-12-23 02:04:44 +0000

    Fixed export

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@
 init() {
   if [[ ${BASH_SOURCE[0]} != "$0" ]]; then
     printf "%s==>%s %s Script started in source mode:%s %s\\n" "${GREEN}" "${ALL_OFF}" "${BOLD}" "$0" "${ALL_OFF}"
-    export -f "{$0}"
+    export -f "$(basename "${BASH_SOURCE[0]}"::main)"
   else
     printf "%s==>%s %s Script started in execute mode:%s %s\\n" "${GREEN}" "${ALL_OFF}" "${BOLD}" "$0" "${ALL_OFF}"
  $(basename "$0::main") "${@}"
