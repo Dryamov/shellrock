6fc2d9bc4f19c10cd7da00e2eaf498dd9a022fb5
Vyacheslav Dryamov <dryamov.dev@gmail.com>
2018-12-23 04:16:03 +0000

    Added some parse opts

--- main.sh
+++ main.sh
@@ -3,8 +3,42 @@
 #main.sh
 
 main() {
-  printf "%s==>%s%s This package under construction %s\\n" "${RED}" "${ALL_OFF}" "${BOLD}" "${ALL_OFF}"
-  echo "${@}"
-  echo "$*"
-  return 0
+	set -o errexit
+#	set -o nounset
+	set -o pipefail
+	IFS=$'\n\t'
+
+	local parametr
+	local argument
+	local action
+
+	while [ "$#" -gt 0 ]; do
+		parametr="$1"
+		shift
+		case $parametr in
+		-h | --help | help)
+			$(basename "$0::usage")
+			exit 0
+			;;
+		-d | --debug)
+			set -o xtrace
+			;;
+		-* | --*)
+			printf "Invalid parametr: %s\\n" "$parametr"
+			exit 1
+			;;
+		*)
+			if ! command -v $(basename "$0::$parametr") >/dev/null 2>&1; then
+				echo "wrong command $parametr"
+				exit 1
+			else
+				action="$parametr"
+				[[ -z "$1" ]] && echo "missing argument"
+				shift
+			fi
+			;;
+		esac
+	done
+	echo "$action $argument"
+	return "$?"
 }
---
+++ usage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#usage.sh
+
+usage() {
+        cat << EOF
+Usage:
+        -h|--help       Displays this help
+EOF
+return "$?"
+}
